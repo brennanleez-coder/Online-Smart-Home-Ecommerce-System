551923cd9c9af73642985b20daa72cefd848ba88
xin yen <xinyen1910@gmail.com>
2021-10-07 21:48:16 +0800

    edited syntax

--- purchase.py
+++ purchase.py
@@ -3,6 +3,7 @@ from datetime import date
 def purchase(color, factory, productionYear, powerSupply, customerid, productID):
     sql = "SELECT productID FROM Product where productID= %d"
     val = (productID)
+    mydb.commit()
     mycursor.execute(sql, val)
     myresult = mycursor.fetchall() #product
 
@@ -10,6 +11,7 @@ def purchase(color, factory, productionYear, powerSupply, customerid, productID)
     #getItemID
     sql1 = "SELECT itemID FROM Item WHERE color = %s AND factory = %s AND powerSupply = %s productionYear = %s AND purchaseStatus = "SOLD" ORDER BY RAND() LIMIT 1"
     val1 = [%s, %s, %s, %s]
+    mydb.commit()
     mycursor.execute(sql1,val1)
     itemIDAvailable = mycursor.fetchall()
    
@@ -25,11 +27,11 @@ def purchase(color, factory, productionYear, powerSupply, customerid, productID)
     mydb.commit()
 
     #Update to sold
-    sql3 = "UPDATE Item SET purchaseStatus= "SOLD" , productID = %s WHERE itemID = %s "
-    val3 = [productID, itemIDAvailable]
-
+    sql3 = "UPDATE Item SET purchaseStatus= %s, productID = %s WHERE itemID = %s "
+    val3 = ["SOLD", productID, itemIDAvailable]
+    mycursor.execute(sql1, val1)
     mydb.commit()
 
     flash("Item successfully purchased")
     
-    )
+
